00ba924b1b60a71fe7aea7e8ddd2458d04141686
kavmozen <kav@mozen.ru>
2026-04-10 13:20:52 +0300

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  brandName: "МП-Сверка",
+  subBrand: "nalog.taxi",
+  telegramUrl: "https://t.me/your_username",
+  email: "hello@example.com",
+  phone: "+7 (999) 000-00-00"
+};
